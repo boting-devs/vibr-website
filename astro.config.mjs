5cb2224c6e98ce64721b43992c0ff3c1cf6c9cb9
ooliver1 <oliverwilkes2006@icloud.com>
2022-09-24 13:22:14 +0100

    fix: use soon to be url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://u.rl",
+  site: "https://vibr.boting.xyz",
   integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
 });
