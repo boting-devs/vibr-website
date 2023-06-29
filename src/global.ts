9dba9c4853cfbf4b2450c1d1bf988d89a8d94085
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-06-29 17:58:07 +0100

    refactor: convert price sections into a component

--- src/global.ts
+++ src/global.ts
@@ -2,3 +2,9 @@ import { Stripe } from "stripe";
 export const stripe = new Stripe(import.meta.env.STRIPE_KEY, {
   apiVersion: "2022-11-15",
 });
+
+export interface ProductMetadata {
+  readonly extra_product?: string;
+  readonly extra_description?: string;
+  readonly features: string;
+}
