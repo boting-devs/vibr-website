8d39818c5dc8871de1bf0b5018e73817d17bcc5a
Oliver Wilkes <oliverwilkes2006@icloud.com>
2023-06-29 17:52:46 +0100

    refactor: make premium page dynamic(ish)

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="@astrojs/image/client" />
+
+interface ImportMetaEnv {
+  readonly STRIPE_KEY: string;
+  // more env variables...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
